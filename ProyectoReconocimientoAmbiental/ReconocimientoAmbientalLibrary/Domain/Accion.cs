0bde1e44a03c11792f8dc78ec1eccaa75ff64504
krisqg <kristelqg93@hotmail.com>
2017-06-28 00:06:53 -0600

    Actualizacion Dominio

---
+++ ProyectoReconocimientoAmbiental/ReconocimientoAmbientalLibrary/Domain/Accion.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReconocimientoAmbientalLibrary.Domain
+{
+    public class Accion:Evidencia
+    {
+        private int idAccion;
+        private String detalleAccion;
+        private String informeTecnico;
+
+        public Accion()
+        {
+
+        }//constructor
+
+        public int IdAccion
+        {
+            get
+            {
+                return idAccion;
+            }
+
+            set
+            {
+                idAccion = value;
+            }
+        }
+
+        public string DetalleAccion
+        {
+            get
+            {
+                return detalleAccion;
+            }
+
+            set
+            {
+                detalleAccion = value;
+            }
+        }
+
+        public string InformeTecnico
+        {
+            get
+            {
+                return informeTecnico;
+            }
+
+            set
+            {
+                informeTecnico = value;
+            }
+        }
+    }//Accion
+
+}//namespace
