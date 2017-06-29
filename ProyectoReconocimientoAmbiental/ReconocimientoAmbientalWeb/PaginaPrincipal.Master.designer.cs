76d5872fb8ea51b84d5bdf0a23c623227e6de1a3
krisqg <kristelqg93@hotmail.com>
2017-06-28 19:08:40 -0600

    pagina principal comienzo

--- ProyectoReconocimientoAmbiental/ReconocimientoAmbientalWeb/PaginaPrincipal.Master.designer.cs
+++ ProyectoReconocimientoAmbiental/ReconocimientoAmbientalWeb/PaginaPrincipal.Master.designer.cs
@@ -38,5 +38,23 @@ namespace ReconocimientoAmbientalWeb {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
+        /// Menu1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu Menu1;
+        
+        /// <summary>
+        /// ContentPlaceHolderBody control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolderBody;
     }
 }
