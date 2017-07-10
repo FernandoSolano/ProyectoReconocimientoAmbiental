8430dec8aa253792d2e30f7c77ce71a1b782a781
FernandoSolano <fer_odi10@hotmail.com>
2017-07-10 01:33:56 -0600

    AsignarResponsable functionality added and working

---
+++ ProyectoReconocimientoAmbiental/ReconocimientoAmbientalLibrary/Business/EncargadoEvaluacionBusiness.cs
@@ -0,0 +1,24 @@
+﻿using ReconocimientoAmbientalLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReconocimientoAmbientalLibrary.Business
+{
+    public class EncargadoEvaluacionBusiness
+    {
+        private EncargadoEvaluacionData encargadoEvaluacionData;
+
+        public EncargadoEvaluacionBusiness(String connectionString)
+        {
+            encargadoEvaluacionData = new EncargadoEvaluacionData(connectionString);
+        }
+
+        public void ActualizarEncargadoDeArea(int idArea, int idFuncionario, String nombreEncargado)
+        {
+            encargadoEvaluacionData.ActualizarEncargadoDeArea(idArea, idFuncionario, nombreEncargado);
+        }
+    }
+}
