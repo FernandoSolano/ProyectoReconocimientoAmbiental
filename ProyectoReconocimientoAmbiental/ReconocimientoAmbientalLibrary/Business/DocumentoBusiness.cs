e4402f2adc6de700259924843cdbda18fdbf99e0
Paulino Salazar <lpscoto@gmail.com>
2017-07-10 14:42:29 -0600

    10 julio 2pm evidencias (3)

---
+++ ProyectoReconocimientoAmbiental/ReconocimientoAmbientalLibrary/Business/DocumentoBusiness.cs
@@ -0,0 +1,32 @@
+﻿using ReconocimientoAmbientalLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+
+namespace ReconocimientoAmbientalLibrary.Business
+{
+    public class DocumentoBusiness
+    {
+        private DocumentoData documentoData;
+
+        public DocumentoBusiness(String cadenaConexion)
+        {
+            this.documentoData = new DocumentoData(cadenaConexion);
+        }//constructor
+
+        public void getAll(GridView grid)
+        {
+            documentoData.getAll(grid);
+        }
+
+        public void Insertar(int subcriterio, String titulo, String fechaRealizacion, String tipoEscrito, String detalle,
+                        String fechaEmision, String emisor, string nombreArchivo, string tipo, byte[] archivo)
+        {
+            documentoData.Insertar(subcriterio, titulo, fechaRealizacion, tipoEscrito, detalle, fechaEmision, emisor, nombreArchivo, tipo, archivo);
+        }
+    }
+}
+
---
+++ ProyectoReconocimientoAmbiental/ReconocimientoAmbientalLibrary/Data/NormativaData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+
+namespace ReconocimientoAmbientalLibrary.Data
+{
+    public class NormativaData
+    {
+        private String cadenaConexion;
+
+        public NormativaData(String cadenaConexion)
+        {
+            this.cadenaConexion = cadenaConexion;
+        }//constructor
+
+        public void getAll(GridView grid)
+        {
+            SqlConnection sqlConnection1 = new SqlConnection(cadenaConexion);
+            SqlCommand cmd;
+            sqlConnection1.Open();
+            cmd = new SqlCommand("mostrar_normativas", sqlConnection1);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            grid.DataSource = ds;
+            grid.DataBind();
+            cmd.ExecuteNonQuery();
+            sqlConnection1.Close();
+        }
+
+        public void Insertar(int idSubcriterio, String titulo, String fecha, String detalle, string nombreArchivo, string tipo, byte[] archivo)
+        {
+            SqlConnection connection = new SqlConnection(cadenaConexion);
+            connection.Open();
+            SqlCommand cmd = new SqlCommand("sp_nueva_normativa", connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@s_tituloEvidencia", titulo);
+            cmd.Parameters.AddWithValue("@s_fechaEvidencia", fecha);
+            cmd.Parameters.AddWithValue("@s_idSubcriterio", idSubcriterio);
+            cmd.Parameters.AddWithValue("@s_detalle", detalle);
+            cmd.Parameters.AddWithValue("@s_nombreArchivo", nombreArchivo);
+            cmd.Parameters.AddWithValue("@s_file", archivo);
+            cmd.Parameters.AddWithValue("@s_tipoArchivo", tipo);
+
+            cmd.ExecuteNonQuery();
+        }
+
+    }
+}
